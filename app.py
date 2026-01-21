81ec20aacf9f195dcf452cac00e20411b16efad6
Manish4325 <manishroyal@rbihub.in>
2026-01-21 13:18:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime, date
-
+#
 # =====================================================
 # PAGE CONFIG
 # =====================================================
